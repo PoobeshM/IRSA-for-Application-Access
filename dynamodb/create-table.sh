8a1e795b1500470f0c105ef96e157048a4292cca
PoobeshM <mpoobesh123@gmail.com>
2026-06-25 13:49:20 +0530

    Access DynamoDB

--- dynamodb/create-table.sh
+++ dynamodb/create-table.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-# TODO: create DynamoDB table
+
+aws dynamodb create-table \
+  --table-name Employee \
+  --attribute-definitions \
+      AttributeName=EmpId,AttributeType=S \
+  --key-schema \
+      AttributeName=EmpId,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST \
+  --region us-east-1
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -9,24 +9,25 @@ module "eks" {
 
   enable_irsa = true
 
+  # Give the Terraform caller (Github-Actions-User) cluster admin access
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id = module.vpc.vpc_id
 
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type       = "AL2023_x86_64_STANDARD"
-    instance_types = ["t3.medium"]
+    instance_types = ["t3.micro"]
   }
 
   eks_managed_node_groups = {
-
     default = {
-
       min_size     = 2
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"]
 
       capacity_type = "ON_DEMAND"
 
